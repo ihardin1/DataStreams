6f065bdfe00d280f380a426f9dcd46426e8d9762
India Hardin <indiahardin@Indias-Air.lan>
2023-04-19 22:50:33 -0400

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        JFrame DataStreams = new DataStreams();
+        DataStreams.setVisible(true);
+    }
+}
